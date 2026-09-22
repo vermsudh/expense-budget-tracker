65c6939c1de5f454f490d048466b9281fe4b0d5e
sudhanshu verma <65793304+bunny-shinchan@users.noreply.github.com>
2026-09-22 13:19:24 +0530

    Add resource route for expenses using ExpenseController

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExpenseController;
 
 Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+
+    Route::resource('expenses', ExpenseController::class);
 });
 
 require __DIR__.'/settings.php';
